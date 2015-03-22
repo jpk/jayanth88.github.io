9301afda00cbc7fee5fe5ba6cc7a956fd57aa2a5
Jayanth Parayil Kumarji <jp972@cornell.edu>
2015-03-21 22:52:53 -0700

    adding googlt analytics

--- js/scripts.js
+++ js/scripts.js
@@ -14,8 +14,19 @@ $(document).ready(function()
 	shufflePeriodically();
 	populateSkills();
 	activeNavBarOptions();
+	loadGAnalytics();
 });
 
+
+function loadGAnalytics(){
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	})(window,document,'script','http://www.google-analytics.com/analytics.js','ga');
+
+	  ga('create', 'UA-61051244-1', 'auto');
+	  ga('send', 'pageview');
+}
 
 function shufflePeriodically(){
 	var nameSpace = $("#shuffleMe");
@@ -44,8 +55,10 @@ function applyResize()
 }
 
 function setupColorDivision(divColors){
-	for(var i=0;i<divColors.length;i++){
-		changeBackgroundColor($(divColors[i][0]),divColors[i][1]);
+	if(divColors){
+		for(var i=0;i<divColors.length;i++){
+			changeBackgroundColor($(divColors[i][0]),divColors[i][1]);
+		}
 	}
 }
 
